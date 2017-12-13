78fb294d5eefaea7db2bc47f63584e17291a352e
Jimmy Yen <tw.haoto@gmail.com>
2017-12-13 11:22:24 +0800

    Add option to defer loop closure detection until FinishTrajectory().

--- cartographer/mapping/pose_graph.cc
+++ cartographer/mapping/pose_graph.cc
@@ -110,6 +110,8 @@ proto::PoseGraphOptions CreatePoseGraphOptions(
   options.set_global_constraint_search_after_n_seconds(
       parameter_dictionary->GetDouble(
           "global_constraint_search_after_n_seconds"));
+  options.set_defer_loop_closure_detection(
+      parameter_dictionary->GetBool("defer_loop_closure_detection"));
   PopulateOverlappingSubmapsTrimmerOptions2D(&options, parameter_dictionary);
   return options;
 }
