ec8b90cc088e09b9f7fff5d15d63116fc7f11b19
qiao <qiao.helloworld@gmail.com>
2018-10-03 17:06:19 +0800

    feat: pbstream: New tool to extract info from pbstream input and export to file

--- cartographer/io/pbstream_main.cc
+++ cartographer/io/pbstream_main.cc
@@ -18,6 +18,7 @@
 #include <unordered_set>
 
 #include "cartographer/io/internal/pbstream_info.h"
+#include "cartographer/io/internal/pbstream_export.h"
 #include "cartographer/io/internal/pbstream_migrate.h"
 #include "gflags/gflags.h"
 #include "glog/logging.h"
@@ -30,7 +31,8 @@ int main(int argc, char** argv) {
       "Swiss Army knife for pbstreams.\n\n"
       "Currently supported subcommands are:\n"
       "\tinfo    - Prints summary of pbstream.\n"
-      "\tmigrate - Migrates old pbstream (w/o header) to new pbstream format.";
+      "\tmigrate - Migrates old pbstream (w/o header) to new pbstream format.\n"
+      "\texport  - Export information to file.";
   google::ParseCommandLineFlags(&argc, &argv, true);
 
   if (argc < 2) {
@@ -41,6 +43,8 @@ int main(int argc, char** argv) {
     return ::cartographer::io::pbstream_info(argc, argv);
   } else if (std::string(argv[1]) == "migrate") {
     return ::cartographer::io::pbstream_migrate(argc, argv);
+  } else if (std::string(argv[1]) == "export") {
+    return ::cartographer::io::pbstream_export(argc, argv);
   } else {
     LOG(INFO) << "Unknown subtool: \"" << argv[1];
     google::SetUsageMessage(usage_message);
